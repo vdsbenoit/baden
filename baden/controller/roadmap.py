8f51d300443d127c8778e7fd505e4e2d5754f2da
Benoit Vander Stappen <benoit.vanderstappen@sony.com>
2019-04-18 17:49:45 +0200

    change document generation method to docxtpl

--- baden/controller/roadmap.py
+++ baden/controller/roadmap.py
@@ -4,14 +4,19 @@ import os
 import tempfile
 
 import pyqrcode
+from docx import Document as Document_compose
+from docx.shared import Mm
+from docxcompose.composer import Composer
+from docxtpl import DocxTemplate, InlineImage
 from mailmerge import MailMerge
 
+from model import service, properties
 from model.game import Game
 from model.team import Team
-from model import service, properties
 
 log = logging.getLogger('default')
 
+LOOP_TEMPLATE = os.path.join(properties.DATA_DIR, "loop_template.docx")
 TEAM_ROADMAP_TEMPLATE = os.path.join(properties.DATA_DIR, "team_roadmap_template.docx")
 GAME_ROADMAP_TEMPLATE = os.path.join(properties.DATA_DIR, "game_roadmap_template.docx")
 QR_SCALE = 4
@@ -21,54 +26,68 @@ QR_VERSION = 4
 QR_ERROR = 'Q'
 
 
-def generate_team_roadmaps():
-    tb_merging = []
+def _combine_docx(target_file, files):
+    master = Document_compose(files[0])
+    composer = Composer(master)
+    for i in range(1, len(files)):
+        doc_temp = Document_compose(files[i])
+        composer.append(doc_temp)
+    composer.save(target_file)
+
+
+def generate_team_roadmaps(target_file):
+    files_to_merge = list()
     with tempfile.TemporaryDirectory() as tmpdir:
         for team in Team.objects():
-            team_dict = dict(section=team.section,
-                             teamCode=team.code,
-                             )
+            tpl = DocxTemplate(TEAM_ROADMAP_TEMPLATE)
+
             qr = pyqrcode.create(team.hash, error=QR_ERROR, version=QR_VERSION)
             qr_file = os.path.join(tmpdir, "team.png")
             qr.png(qr_file, scale=QR_SCALE, module_color=QR_COLOR, quiet_zone=QUIET_ZONE)
-            with open(qr_file, 'r', encoding='ANSI') as f:
-                img_data = f.read()
-            team_dict['IMAGE:qrCode'] = img_data
+
+            context = dict(
+                section=team.section,
+                teamCode=team.code,
+                qrCode=InlineImage(tpl, qr_file, width=Mm(37))
+            )
             games = service.get_games(team.code)
-            for i in range(len(games)):
-                team_dict["game{}".format(i+1)] = games[i].name
-                team_dict["gID{}".format(i+1)] = str(games[i].number)
-            tb_merging.append(team_dict)
+            for i, game in enumerate(games, 1):
+                context["game{}".format(i)] = games.name
+                context["gId{}".format(i)] = str(games.number)
 
-        with MailMerge(TEAM_ROADMAP_TEMPLATE) as document:
-            document.merge_templates(tb_merging, separator='page_break')
-            document.write(os.path.join(properties.PROJECT_ROOT, "team_roadmaps.docx"))
+            tpl.render(context)
+            team_file = os.path.join(tmpdir, "team{}.docx".format(team.code))
+            tpl.save(team_file)
+            files_to_merge.append(team_file)
+
+        _combine_docx(target_file, files_to_merge)
 
 
-def generate_game_roadmaps():
-    tb_merging = []
+def generate_game_roadmaps(target_file):
+    files_to_merge = list()
     with tempfile.TemporaryDirectory() as tmpdir:
         game_names = Game.objects().distinct('name')
         for name in game_names:
             game = Game.objects(name=name).first()
-            game_dict = dict(gameName=name,
-                             gID=str(game.number),
-                             circuit=game.circuit)
+            tpl = DocxTemplate(GAME_ROADMAP_TEMPLATE)
+
             qr = pyqrcode.create(game.hash, error=QR_ERROR, version=QR_VERSION)
             qr_file = os.path.join(tmpdir, "game.png")
             qr.png(qr_file, scale=QR_SCALE, module_color=QR_COLOR, quiet_zone=QUIET_ZONE)
-            with open(qr_file, 'r') as f:
-                img_data = f.read()
-            game_dict['IMAGE:qrCode'] = img_data
 
-            i = 1
-            for players in service.get_players(game.number):
-                game_dict["teams{}".format(i)] = "{} - {}".format(players[0].code, players[1].code)
-                i += 1
-            tb_merging.append(game_dict)
+            context = dict(
+                gameName=name,
+                gID=str(game.number),
+                circuit=game.circuit,
+                qrCode=InlineImage(tpl, qr_file, width=Mm(37))
+            )
 
-        with MailMerge(GAME_ROADMAP_TEMPLATE) as document:
-            document.merge_pages(tb_merging)
-            document.write(os.path.join(properties.PROJECT_ROOT, "game_roadmaps.docx"))
+            for i, players in enumerate(service.get_players(game.number), 1):
+                context["teams{}".format(i)] = "{} - {}".format(players[0].code, players[1].code)
 
+            tpl.render(context)
+            game_file = os.path.join(tmpdir, "team{}.docx".format(team.code))
+            tpl.save(game_file)
+            files_to_merge.append(game_file)
 
+        _combine_docx(target_file, files_to_merge)
--- baden/documents.py
+++ baden/documents.py
@@ -5,9 +5,11 @@ Baden Battle scores tool.
 Documents generation (roadmaps & tables)
 """
 import logging
+import os
+
 import controller.util
 import controller.roadmap
-
+from model import properties
 
 __author__ = 'Benoit Vander Stappen'
 
@@ -16,5 +18,5 @@ log = logging.getLogger('default')
 
 if __name__ == "__main__":
     controller.util.initialize()
-    controller.roadmap.generate_team_roadmaps()
-    # controller.roadmap.generate_game_roadmaps()
+    controller.roadmap.generate_team_roadmaps(os.path.join(properties.PROJECT_ROOT, "team_roadmaps.docx"))
+    # controller.roadmap.generate_game_roadmaps(os.path.join(properties.PROJECT_ROOT, "game_roadmaps.docx"))
